6183a48efd05e8296da1c707d06c5857f3cfd4d2
al Mohamady <al.Mohamady47@gmail.com>
2020-05-16 00:42:26 +0200

    Passing Dynamic data in Html

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Destination:
+    id: int 
+    name: str
+    img: str 
+    des: str
+    price: int 
+    spical: bool
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = 'Hurghada'
+    dest1.des = 'It is one of the country\'s main tourist centers located on the Red Sea coast.'
+    dest1.price = 200
+
+    return render(request, 'index.html', {'dest1': dest1})
