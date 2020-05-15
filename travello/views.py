42cfafd3b82cb590c531162d785b26786c40386e
al Mohamady <al.Mohamady47@gmail.com>
2020-05-16 01:49:18 +0200

    If Statement

--- travello/views.py
+++ travello/views.py
@@ -10,18 +10,21 @@ def index(request):
     dest1.des = 'It is one of the country\'s main tourist centers.'
     dest1.img = 'hurghada.jpg'
     dest1.price = 200
+    dest1.spical = True
 
     dest2 = Destination()
     dest2.name = 'Cairo'
     dest2.des = 'Cairo is the capital of Egypt .'
     dest2.img = 'cairo.jpg'
     dest2.price = 400
+    dest2.spical = False
 
     dest3 = Destination()
     dest3.name = 'Alexandria'
     dest3.des = 'Alexandria is the second-largest city in Egypt.'
     dest3.img = 'alexandria.jpg'
     dest3.price = 500
+    dest3.spical = True
 
     dest = [ dest1, dest2, dest3 ]
 
