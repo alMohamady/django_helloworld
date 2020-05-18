8cafe5803ab9816d2ba1751d06af0d7282ce9486
al Mohamady <al.Mohamady47@gmail.com>
2020-05-19 00:53:54 +0200

    Re-Migration

--- travello/models.py
+++ travello/models.py
@@ -7,5 +7,5 @@ class Destination(models.Model):
     name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics')
     des = models.TextField()
-    price = models.IntegerField 
+    price = models.IntegerField() 
     spical = models.BooleanField(default=False)
