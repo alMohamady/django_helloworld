4ff11fdbb88730b381c611e20a0e11cc581ab0bf
al Mohamady <al.Mohamady47@gmail.com>
2020-05-19 00:03:28 +0200

    Using Models and Migrations 1

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hello_world',
+        'USER': 'postgres', 
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
--- travello/models.py
+++ travello/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Destination:
-    id: int 
-    name: str
-    img: str 
-    des: str
-    price: int 
-    spical: bool
+class Destination(models.Model):
+    name: models.CharField(max_lenght=100)
+    img: models.CharField(max_lenght=100) 
+    des: models.TextField()
+    price: models.IntegerField 
+    spical: models.BooleanField
